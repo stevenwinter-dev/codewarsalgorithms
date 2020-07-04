73dee57f0d2b7001e108a7f64fb9991c106e8e22
Steven Winter <stevenwinter.me@gmail.com>
2020-07-04 18:26:47 -0500

    added new algorithm

--- code wars algorithms.js
+++ code wars algorithms.js
@@ -1,17 +1,16 @@
-
-
 //Multiply 2 nums
-
 function multiply(a, b){
     return a * b
 }
 
 //Capitalize first letter of each word
-
 String.prototype.toJadenCase = function () {
     const strSplit = this.split(' ')
     return strSplit.map( word => word[0].toUpperCase() + word.substr(1)).join(' ')
 }
+    //Input:
+    // var str = "How can mirrors be real if our eyes aren't real";
+    // Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
 
 //Find the highest and lowest number in a string of numbers
 function highAndLow(numbers){
@@ -20,3 +19,30 @@ function highAndLow(numbers){
     const low = Math.min.apply(null, nums);
     return high + ' ' + low;
   }
+    //Input:
+    // highAndLow("1 2 3 4 5");  // return "5 1"
+    // highAndLow("1 2 -3 4 5"); // return "5 -3"
+    // highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+  //Take an array of names. Create sentences with names based on how many are input.
+  function likes(names) {
+    if (names.length === 1) {
+      return `${names[0]} likes this`;
+    } else if (names.length === 2){
+        return `${names[0]} and ${names[1]} like this`;  
+    } else if (names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`; 
+    } else if (names.length > 3) {
+        return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;
+    } else {
+        return 'no one likes this';
+    }
+  }
+
+    //Input:
+    // likes [] // must be "no one likes this"
+    // likes ["Peter"] // must be "Peter likes this"
+    // likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+    // likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+    // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+  
